82c31dd81400d6dc019c7116c2ea4ee3aa52a1e1
Jessica Peck <jessypeck@gmail.com>
2021-04-20 11:21:14 -0700

    Add sandbox dir for development

---
+++ sandbox/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TextSwap from "react-text-swap";
+
+function App() {
+  return (
+    <div className="App">
+      <TextSwap></TextSwap>
+    </div>
+  );
+}
+
+export default App;
