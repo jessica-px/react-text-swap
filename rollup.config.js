26d9f65426d777108413314876877902566f09e2
Jessica Peck <jessypeck@gmail.com>
2021-04-20 11:20:57 -0700

    Initialize repo

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.tsx",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      file: pkg.module,
+      format: "es",
+    },
+  ],
+  external: ["react", "react-dom"],
+  plugins: [typescript({ objectHashIgnoreUnknownHack: true })],
+};
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const TextSwap = () => {
+  return <p>Hello World</p>;
+};
+
+export default TextSwap;
